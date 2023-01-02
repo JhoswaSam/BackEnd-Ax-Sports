ec4f2cd386868346cd6e9cc3ada90deec933daf5
JhoswaSam <jhoswasamanez@gmail.com>
2023-01-02 15:56:18 -0500

    edit readme and complete security horario

--- README.md
+++ README.md
@@ -7,18 +7,31 @@ Proyecto personal para el desarrollo de una plataforma enfocada a las academias 
 Deyvis Jhoswa Samanez Ccoscco
 
 
-*** LISTA DE PERMISOS ***
+## LISTA DE PERMISOS
 
 |  ENTIDADES       |         Super        |       Docente       |     Estudiante      |     Responsable     |  
 |------------------|----------------------|---------------------|---------------------|---------------------|
-|  Administrador   |  []L []B []C []E []D | []P []EP-T          |                     |                     |
-|  Distancia       |  []L []B []C []E []D | []L []B []C []E     | []L                 | []L                 |
-|  Estudiante      |  []L []B []C []E []D | []L []B []C []E     | []P []EP            | []LE                |
-|  Evento          |  []L []B []C []E []D | []L []B []C []E     | []L                 |                     |
-|  Horario         |  []L []B []C []E []D | []L []B []C []E     | []L                 | []L                 |
-|  Inscripcion     |  []L []B []C []E []D |                     | DESARROLLO          | DESARROLLO          |
-|  Pago            |  []L []B []C []E []D |                     | []L []B []C []E     | []L []B []C []E     |
-|  Puntaje         |  []L []B []C []E []D | []L []B []C []E     | []L []B []C []E     |                     |
-|  Responsable     |  []L []B []C []E []D | []L []B []C []E     | []LR                | []E []EP            |
-|  Sede            |  []L []B []C []E []D | []L []B []C []E []D |                     |                     |
-|  Tipo            |  []L []B []C []E []D |                     |                     |                     |
+|  Administrador   |  L B C E D           | P EP-T              |                     |                     |
+|  Distancia       |  L B C E D           | L B C E             | L                   | L                   |
+|  Estudiante      |  L B C E D           | L B C E             | P EP                | LE                  |
+|  Evento          |  L B C E D           | L B C E             | L                   |                     |
+|  Horario         |  L B C E D           | L B C E             | L                   | L                   |
+|  Inscripcion     |  L B C E D           |                     | DESARROLLO          | DESARROLLO          |
+|  Pago            |  L B C E D           |                     | L B C E             | L B C E             |
+|  Puntaje         |  L B C E D           | L B C E             | L B C E             |                     |
+|  Responsable     |  L B C E D           | L B C E             | LR                  | E EP                |
+|  Sede            |  L B C E D           | L B C E D           |                     |                     |
+|  Tipo            |  L B C E D           |                     |                     |                     |
+
+### LEYENDA
+
+L       -  Listar
+B       -  Buscar por Id
+C       -  Crear 
+E       -  Editar/Actualizar
+D       -  Eliminar
+P       -  Perfil
+EP-T    -  Editar/Actualizar perfil(para administradores sin cambiar el tipo)
+EP      -  Editar
+LE      -  Listar estudiantes(solo para responsables que tengan estudiantes asigandos)
+LR      -  Listar responsable(solo para estudiantes que tengan un responsble asignado)
--- src/controllers/horario.controller.ts
+++ src/controllers/horario.controller.ts
@@ -17,7 +17,7 @@ export class HorarioController{
             if (!token) {
                 return this.httpResponse.Unauthorized(res,"Inicie sesion primero");
             }
-            
+
             const data = await this.horarioService.findAll();
             if (data.length === 0) {
                 return this.httpResponse.NotFound(res, "No existen datos")
@@ -31,11 +31,21 @@ export class HorarioController{
     async getHorarioById(req: Request, res: Response) {
         const {id}= req.params;
         try {
-            const data = await this.horarioService.findbyid(id);
-            if (!data) {
-                return this.httpResponse.NotFound(res, "No existe datos")
+            // Verificamos si esta logeado o no 
+            const token = req.cookies.accessToken
+            if (!token) {
+                return this.httpResponse.Unauthorized(res,"Inicie sesion primero");
             }
-            return this.httpResponse.Ok(res, data)
+
+            const typeUser = await this.auth.setPermissions(token);
+            if (typeUser === 1 || typeUser === 2) {
+                const data = await this.horarioService.findbyid(id);
+                if (!data) {
+                    return this.httpResponse.NotFound(res, "No existe datos")
+                }
+                return this.httpResponse.Ok(res, data)
+            }
+            return this.httpResponse.Unauthorized(res,"No tiene permiso para esta accion");
         } catch (e) {
             return this.httpResponse.NotFound(res, e)
         }
@@ -43,8 +53,19 @@ export class HorarioController{
 
     async createHorario(req: Request, res: Response) {
         try {
-            const data = await this.horarioService.create(req.body);
-            return this.httpResponse.Ok(res, data)
+            // Verificamos si esta logeado o no 
+            const token = req.cookies.accessToken
+            if (!token) {
+                return this.httpResponse.Unauthorized(res,"Inicie sesion primero");
+            }
+
+            const typeUser = await this.auth.setPermissions(token);
+            if (typeUser === 1 || typeUser === 2) {
+
+                const data = await this.horarioService.create(req.body);
+                return this.httpResponse.Ok(res, data)
+            }
+            return this.httpResponse.Unauthorized(res,"No tiene permiso para esta accion");
         } catch (e) {
             return this.httpResponse.NotFound(res, e)
         }
@@ -53,13 +74,24 @@ export class HorarioController{
     async updateHorario(req: Request, res: Response) {
         const {id}= req.params;
         try {
-            const data = await this.horarioService.update(id,req.body);
-            
-            if (!data.affected) {
-                return this.httpResponse.NotFound(res, "Hay un error al actualizar")
+            // Verificamos si esta logeado o no 
+            const token = req.cookies.accessToken
+            if (!token) {
+                return this.httpResponse.Unauthorized(res,"Inicie sesion primero");
             }
 
-            return this.httpResponse.Ok(res, data)
+            const typeUser = await this.auth.setPermissions(token);
+            if (typeUser === 1 || typeUser === 2) {
+                const data = await this.horarioService.update(id,req.body);
+                
+                if (!data.affected) {
+                    return this.httpResponse.NotFound(res, "Hay un error al actualizar")
+                }
+    
+                return this.httpResponse.Ok(res, data)
+            }
+            
+            return this.httpResponse.Unauthorized(res,"No tiene permiso para esta accion");
         } catch (e) {
             return this.httpResponse.NotFound(res, e)
         }
@@ -68,13 +100,24 @@ export class HorarioController{
     async deteleHorario(req: Request, res: Response) {
         const {id}= req.params;
         try {
-            const data = await this.horarioService.delete(id);
-            
-            if (!data.affected) {
-                return this.httpResponse.NotFound(res, "Hay un error al actualizar")
+            // Verificamos si esta logeado o no 
+            const token = req.cookies.accessToken
+            if (!token) {
+                return this.httpResponse.Unauthorized(res,"Inicie sesion primero");
             }
 
-            return this.httpResponse.Ok(res, data)
+            const typeUser = await this.auth.setPermissions(token);
+            if (typeUser === 1 ) {
+                const data = await this.horarioService.delete(id);
+                
+                if (!data.affected) {
+                    return this.httpResponse.NotFound(res, "Hay un error al eliminar")
+                }
+    
+                return this.httpResponse.Ok(res, data)
+            }
+            
+            return this.httpResponse.Unauthorized(res,"No tiene permiso para esta accion");
         } catch (e) {
             return this.httpResponse.NotFound(res, e)
         }
